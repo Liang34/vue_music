b0726e04ec81ed62d96f3265a5a76b758c25fc7c
Liang34 <1160685230@qq.com>
2022-01-21 15:07:58 +0800

    init

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,2 +1,29 @@
 import axios from 'axios'
 
+const fetch = axios.create({
+  withCredentials: true,
+  timeout: 50000
+})
+fetch.interceptors.request.use(
+  async (config) => {
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+fetch.interceptors.response.use(
+  (axiosRes) => {
+    if (axiosRes.data.result !== 0) {
+      return axiosRes.data
+    } else {
+      return axiosRes.data
+    }
+  },
+  (axiosErr) => {
+    return axiosErr.response
+  }
+)
+
+export default fetch
