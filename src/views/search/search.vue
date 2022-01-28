71c26ce544730def206a522bcec3ef40cec030f1
Liang34 <1160685230@qq.com>
2022-01-28 23:58:17 +0800

    search

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="search">
-    <div class="search-input-wrapper">
-      <search-box v-model="query" @query="onQueryChange" ></search-box>
-    </div>
+  <transition name="search">
+    <div class="search">
+      <div class="search-input-wrapper">
+        <i class="fa iconfont icon-fanhui" @click="back"></i>
+        <search-box v-model="query" @query="onQueryChange" ></search-box>
+      </div>
     <scroll ref="scrollRef" class="search-content">
       <div>
         <div class="search-hots" v-show="!query">
@@ -33,6 +35,7 @@
         </div>
       </div>
     </scroll>
+    </div>
     <!-- <div class="search-result" v-show="query">
       <suggest
         :query="query"
@@ -45,7 +48,7 @@
         <component :is="Component" :data="selectedSinger"/>
       </transition>
     </router-view> -->
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -87,62 +90,124 @@ export default {
 
 <style lang="scss" scoped>
 @import "common/style/variable";
-@import "common/style/mixin";
+.search-enter-active, .search-leave-active {
+  transition: all 0.3s;
+}
+.search-enter, .search-leave-to {
+  transform: translate3d(50%, 0, 0);
+  opacity: 0;
+}
 .search {
   position: fixed;
+  z-index: 100;
   width: 100%;
-  top: 88px;
+  top: 0;
   bottom: 0;
-  display: flex;
-  flex-direction: column;
-  .search-input-wrapper {
-    margin: 20px;
+  background: $color-background;
+  .search-box-wrapper {
+    padding: 10px 40px 10px 43px;
+    // margin-bottom: 15px;
+    background: $color-theme;
+    .fa {
+      position: absolute;
+      left: 5px;
+      top: 3px;
+      padding: 4px 3px;
+      font-size: 30px;
+      color: #fff;
+    }
   }
-  .search-content {
-    flex: 1;
+  .search-scroll-wrapper {
+    height: 100%;
     overflow: hidden;
     .search-hots {
-      margin: 0 20px 0px 20px;
-      .title {
-        margin-bottom: 20px;
-        /* font-size: $font-size-medium; */
-        /* color: $color-text-l; */
-      }
-      .search-hots-item {
-        display: inline-block;
-        padding: 5px 6px;
-        margin: 0 20px 10px 0;
-        border-radius: 6px;
-        background: $color-highlight-background;
-        font-size: $font-size-medium;
-        /* color: $color-text-d; */
-      }
-    }
-    .search-history {
-      position: relative;
       margin: 0 20px;
       .title {
-        display: flex;
-        align-items: center;
-        height: 40px;
-        font-size: $font-size-large;
-        /* color: $color-text-l; */
-        .text {
-          flex: 1;
-        }
-        .clear {
-          @include extend-click();
-          .icon-clear {
+        padding: 15px 5px 0 5px;
+        height: 30px;
+        line-height: 30px;
+        font-size:$font-size-small-x;
+        color: $color-text-g;
+      }
+      span {
+        display: inline-block;
+        padding: 3px 5px;
+        margin: 4px 4px;
+        border: 0.8px solid $color-text-ggg;
+        border-radius: 5px;
+        line-height: 20px;
+        color: $color-text;
+        font-size: $font-size-medium;
+      }
+    }
+    .shortcut-wrapper {
+      position: relative;
+      // top: 178px
+      // bottom: 0
+      // width: 90%
+      margin: 0 auto;
+      .shortcut {
+        height: 100%;
+        overflow: hidden;
+        .hot-key {
+          margin: 0 20px 20px 20px;
+          .title {
+            margin-bottom: 20px;
             font-size: $font-size-medium;
-            /* color: $color-text-d; */
+            color: $color-text;
+          }
+          .item {
+            display: inline-block;
+            padding: 5px 10px;
+            margin: 0 20px 10px 0;
+            border-radius: 6px;
+            background: $color-highlight-background;
+            font-size: $font-size-medium;
+            color: $color-text;
+          }
+        }
+      }
+      .search-history {
+        position: relative;
+        margin: 10px 25px;
+        .title {
+          display: flex;
+          align-items: center;
+          height: 30px;
+          font-size: $font-size-small-x;
+          color: $color-text-g;
+          .text {
+            // font-size: 13px;
+            flex: 1;
+          }
+          .clear {
+            // 扩展点击区域
+            position: relative;
+            &:before {
+                content: '';
+                position: absolute;
+                top: -10px;
+                left: -10px;
+                right: -10px;
+                bottom: -10px;
+            }
+            .icon-clear {
+              font-size: $font-size-medium;
+              color: $color-text;
+            }
           }
         }
       }
     }
+    .search-result {
+      position: relative;
+      width: 100%;
+      top: 10px;
+      bottom: 0;
+    }
   }
-  .search-result {
-    flex: 1;
-    overflow: hidden;
-  }
+}
+.router-view {
+  z-index: 1000;
 }
 </style>
